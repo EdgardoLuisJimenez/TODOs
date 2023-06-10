4aec806405ebb0db66b8116a7d970478fedcf0fe
Edgardo Luis Jimenez <edgardotecno@gmail.com>
2023-06-10 12:27:10 -0500

    Add the addEventListener

--- src/TodoSearch/TodoSearch.js
+++ src/TodoSearch/TodoSearch.js
@@ -1,7 +1,12 @@
 import './TodoSearch.css'
 
 function TodoSearch() {
-  return <input placeholder="Cortar cebolla" className="TodoSearch" />;
+  return <input placeholder="Cortar cebolla" className="TodoSearch" onChange={(event) => {
+    console.log('Escribistes en el TodoSearch');
+    console.log(event);
+    console.log(event.target);
+    console.log(event.target.value);
+  }}/>;
 }
 
 export { TodoSearch };
