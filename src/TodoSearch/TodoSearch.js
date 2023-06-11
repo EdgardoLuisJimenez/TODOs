3e7e71208f660f3ba96e3faf3bc8de0d20661e80
Edgardo Luis Jimenez <edgardotecno@gmail.com>
2023-06-11 10:34:50 -0500

    events on react

--- src/TodoSearch/TodoSearch.js
+++ src/TodoSearch/TodoSearch.js
@@ -1,12 +1,18 @@
-import './TodoSearch.css'
+import "./TodoSearch.css";
 
 function TodoSearch() {
-  return <input placeholder="Cortar cebolla" className="TodoSearch" onChange={(event) => {
-    console.log('Escribistes en el TodoSearch');
-    console.log(event);
-    console.log(event.target);
-    console.log(event.target.value);
-  }}/>;
+  return (
+    <input
+      placeholder="Cortar cebolla"
+      className="TodoSearch"
+      onChange={(event) => {
+        console.log("Escribistes en el TodoSearch");
+        console.log(event);
+        console.log(event.target);
+        console.log(event.target.value);
+      }}
+    />
+  );
 }
 
 export { TodoSearch };
