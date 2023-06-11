2489edb449e73ed2b3b16915e46f081fdebe7482
Edgardo Luis Jimenez <edgardotecno@gmail.com>
2023-06-11 11:53:52 -0500

    Contando TODOs

--- src/App/App.js
+++ src/App/App.js
@@ -4,20 +4,26 @@ import { TodoList } from "../TodoList/TodoList";
 import { TodoItem } from "../TodoItem/TodoItem";
 import { CreateTodoButton } from "../CreateTodoButton/CreateTodoButton";
 import React from "react";
-import './App.css'
+import "./App.css";
 
 const defaultTodos = [
   { text: "Cortar cebolla", completed: true },
   { text: "Tomar el Curso de Intro a React.js", completed: false },
   { text: "Llorar con la Llorona", completed: false },
   { text: "LALALALALALA", completed: false },
+  { text: "Usar Estados derivados", completed: true },
 ];
 
 function App() {
-  return (
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState("");
+
+  const completedTodos = todos.filter((todo) => todo.completed).length;
+  const totalTodos = todos.length;
+
     <>
-      <TodoCounter completed={16} total={25} />
-      <TodoSearch />
+      <TodoCounter completed={completedTodos} total={totalTodos} />
+      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
 
       <TodoList>
         {defaultTodos.map((todo) => (
