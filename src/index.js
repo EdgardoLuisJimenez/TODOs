5c9451de87e020c308c0d9d063b63de3d84e66a1
Edgardo Luis Jimenez <edgardotecno@gmail.com>
2023-08-06 12:43:13 -0500

    Curso Patrones de Render y Composicion

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App/App';
+import "./index.css";
+import App from "./App/App";
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+import { render } from "react-dom";
+const root = document.getElementById("root");
+render(<App />, root);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
