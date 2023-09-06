3f778a8398103c2a06d51e7fb6eebfa42fad8b14
Edgardo Luis Jimenez <edgardotecno@gmail.com>
2023-09-05 22:01:53 -0500

    Creating the first High Order Components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
 import "./index.css";
-import { App } from "./App/App";
+// import { App } from "./App/App";
+import { render } from "react-dom";
 // import React from 'react';
 // import ReactDOM from 'react-dom/client';
-import { render } from "react-dom";
+
+function App(props) {
+  return (
+    <h1>
+      {props.saludo}, {props.nombre}!
+    </h1>
+  );
+}
+
+function WithSaludo(saludo) {
+  return function WrappedComponentWithSaludo(WrappedComponent) {
+    return function ComponenteDeVerdad(props) {
+      return (
+        <>
+          <WrappedComponent {...props} saludo={saludo}/>
+          <p>Estamos acompañando al WrappedComponent</p>
+        </>
+      );
+    };
+  };
+}
+
+const AppWithSaludo = WithSaludo('Wenas')(App);
+
 const root = document.getElementById("root");
-render(<App />, root);
+render(<AppWithSaludo nombre="Juanita" />, root);
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(<App />);
